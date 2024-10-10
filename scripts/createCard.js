3d522e1a56034a52336c7f8f0089b24886e78165
wildmatthieu <matthieu.lopez@wildcodeschool.com>
2024-10-10 15:05:01 +0200

    implementation de l'ajout dynamique de locations à partir d'un fichier de données

--- scripts/createCard.js
+++ scripts/createCard.js
@@ -1,3 +1,48 @@
-function createCard() {
-  // This function should create a card item
+console.log(houseToRent);
+
+function createCard(aRents, onlyAvailables) {
+  for (const rent of aRents) {
+    if (
+      onlyAvailables === false ||
+      (onlyAvailables === true && rent.available === true)
+    ) {
+      let cardTitle = document.createElement("h2");
+      cardTitle.classList.add("card-title");
+      cardTitle.innerText = rent.name;
+
+      let cardParagraph = document.createElement("p");
+      cardParagraph.classList.add("card-description");
+      cardParagraph.innerText = rent.desc;
+
+      let cardButton = document.createElement("button");
+      cardButton.classList.add("card-button");
+      cardButton.innerText = "I want it !";
+
+      let cardBody = document.createElement("div");
+      cardBody.classList.add("card-body");
+
+      cardBody.appendChild(cardTitle);
+      cardBody.appendChild(cardParagraph);
+      cardBody.appendChild(cardButton);
+
+      let eCard = document.createElement("div");
+      eCard.classList.add("card");
+
+      eCard.appendChild(cardBody);
+
+      let eCards = document.getElementById("cards");
+      eCards.appendChild(eCard);
+    }
+  }
 }
+
+createCard(houseToRent, false);
+
+document.getElementById("available-checkbox").addEventListener("click", () => {
+  alert("coucou !");
+
+  let eCards = document.getElementById("cards");
+  eCards.replaceChildren();
+
+  createCard(houseToRent, true);
+});
